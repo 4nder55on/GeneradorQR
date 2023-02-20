3d3b2de0594c87fc686f8a2a2964325a9111c732
Andersson Pérez <anndersson.perez@hotmail.es>
2023-02-20 19:34:09 +0000

    Subida inicial de los archivos

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+require 'phpqrcode/qrlib.php';
+
+//echo $_GET['d'];
+
+
+//QRcode::png($dato,"img/qr_".$dato.".png",'L',32,5);
+if (isset($_GET['d'])) {
+    QRcode::png($_GET['d']);
+}
+else{
+    echo 'Debe Enviar la variable d';
+}
+
+
+?>
